b3300d7824d9d1493c08d0d504c74ccab4d9adf7
BigRicsoo <big.ricsoo@gmail.com>
2021-09-26 18:06:25 +0200

    Add search options

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,7 +9,13 @@ function App() {
           <div className={styles.logo}>BookShelf</div>
         </div>
       </div>
-      <div className={styles.content}>Content</div>
+      <div className={styles.content}>
+        <div className={styles.srcContainer}>
+          <div className={styles.srcButtonActive}>Éves Bontás</div>
+          <div className={styles.srcButton}>Összes</div>
+        </div>
+        <div className={styles.srcDivider} />
+      </div>
       <div className={styles.footer}></div>
     </div>
   );
