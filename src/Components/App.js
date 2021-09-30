b6a275b66c83afef9a4a21fdb0c91707cd299764
BigRicsoo <big.ricsoo@gmail.com>
2021-09-30 17:18:14 +0200

    Add screen size compatibility, new book covers, improved options
    -Screen size compability with breaking points at: 1024px, 768px, 480px
    -The selectable years are pulled from the Books object
    -New book cover for better demonstration

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,30 +1,36 @@
 import React from "react";
 import styles from "./App.module.scss";
+import BookCover from "../Pictures/BookCover.jpg";
 
 const Books = {
   book0: {
     author: "Author Name0",
     title: "The Title0",
+    year: 2021,
     stars: 1,
   },
   book1: {
     author: "Author Name1",
     title: "The Title1",
+    year: 2019,
     stars: 3,
   },
   book2: {
     author: "Author Name2",
     title: "The Title2",
+    year: 2021,
     stars: 5,
   },
   book3: {
     author: "Author Name3",
     title: "The Title3",
+    year: 2020,
     stars: 4,
   },
   book4: {
     author: "Author Name4",
     title: "The Title4",
+    year: 2018,
     stars: 5,
   },
 };
@@ -36,6 +42,26 @@ function returnStars(nmbrOfStars) {
   for (let index = 0; index < 5 - nmbrOfStars; index++) stars += "☆";
 
   return <div className={styles.stars}>{stars}</div>;
+}
+
+function returnYears() {
+  let array = [];
+  for (let index = 0; index < Object.keys(Books).length; index++) {
+    array.push(Books[Object.keys(Books)[index]].year);
+  }
+
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+
+  array.join();
+  array.reverse().reverse(compareNumbers);
+  array = [...new Set(array)];
+  return array.map((keyName, key) => (
+    <div className={styles.dot}>
+      <div className={styles.year}>{keyName}</div>
+    </div>
+  ));
 }
 
 function App() {
@@ -52,32 +78,21 @@ function App() {
           <div className={styles.srcButton}>Összes</div>
         </div>
         <div className={styles.srcDivider} />
-        <div className={styles.years}>
-          <div className={styles.yearFocus}>2021</div>&nbsp;·&nbsp;
-          <div className={styles.year}>2020</div>&nbsp;·&nbsp;
-          <div className={styles.year}>2019</div>&nbsp;·&nbsp;
-          <div className={styles.year}>2018</div>
-        </div>
-
+        <div className={styles.years}>{returnYears()}</div>
         <div className={styles.bookResults}>
-          <div className={styles.teszt}>2021</div>
+          <div className={styles.bookResultsYear}>2021</div>
           &nbsp;
           <div className={styles.bookResultsNmb}>5</div>
         </div>
-
         <div className={styles.bookContainer}>
           {Object.keys(Books).map((keyName, key) => (
             <div className={styles.booksStars} key={key}>
-              <div className={styles.gridElement}>
-                Author: {Books[keyName].author}
-                <br />
-                Title: {Books[keyName].title}
-              </div>
+              <img src={BookCover} alt="BookCover" />
               {returnStars(Books[keyName].stars)}
             </div>
           ))}
-          <div className={styles.gridElement}>
-            <div className={styles.circle} />
+          <div className={styles.addBook}>
+            <div className={styles.add} />
           </div>
         </div>
       </div>
