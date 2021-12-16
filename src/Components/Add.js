d6fdab06deb9dc8e11cde5bbc0410e3ca12759c4
BigRicsoo <big.ricsoo@gmail.com>
2021-12-16 17:24:43 +0100

    Bug fixes and quality life changes
    - Edit and Add component will appear in the center of the screen,
      instead of the top left corner
    - Replace dropdown with two images in the navigation bar
    - Replace some code from inline to functions for better readability

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -6,87 +6,87 @@ import firebase from "./util/firebase";
 let covers = [];
 
 function Add({ exit, booksData, usersData, lang }) {
-  const [selectedBookID, setSelectedBookID] = useState("");
-  const [books, setBooks] = useState(undefined);
+	const [selectedBookID, setSelectedBookID] = useState("");
+	const [books, setBooks] = useState(undefined);
 
-  useEffect(() => {
-    // Retrive data
-    async function pullData() {
-      const userBooks = [];
-      const allBooks = [];
+	useEffect(() => {
+		async function pullData() {
+			const userBooks = [];
+			const allBooks = [];
 
-      for (let id in usersData) {
-        userBooks.push(usersData[id].bookID);
-      }
+			for (let id in usersData) {
+				userBooks.push(usersData[id].bookID);
+			}
 
-      for (let id in booksData) {
-        allBooks.push(booksData[id].bookID);
-        covers.push(booksData[id].cover);
-      }
+			for (let id in booksData) {
+				allBooks.push(booksData[id].bookID);
+				covers.push(booksData[id].cover);
+			}
+			setBooks(allBooks.filter((val) => !userBooks.includes(val)));
+		}
+		pullData();
+	}, [booksData, usersData]);
 
-      //Execlude books, that the user already have
-      setBooks(allBooks.filter((val) => !userBooks.includes(val)));
-    }
-    pullData();
-  }, [booksData, usersData]);
+	return (
+		<>
+			{books !== undefined && (
+				<div className={addStyles.addComponent}>
+					<div className={addStyles.header}>
+						<div>{lang.addBook}&nbsp;</div>
+						<div onClick={exit} className={addStyles.xButton}>
+							+
+						</div>
+					</div>
+					<div className={addStyles.content}>
+						<div className={addStyles.bookCovers}>
+							{Object.keys(books).map((keyName, key) => (
+								<div>
+									<img
+										key={key}
+										className={addStyles.addImg}
+										src={covers[parseInt(books[key])]}
+										alt="BookCover"
+										onClick={() =>
+											setSelectedBookID(books[key])
+										}
+									/>
+								</div>
+							))}
+						</div>
+					</div>
+					<div className={addStyles.footer}>
+						<button
+							onClick={() => {
+								const userBooksRef = firebase
+									.database()
+									.ref("users/0/books");
 
-  return (
-    <>
-      {/* Wait till tha data arrives */}
-      {books !== undefined && (
-        <div className={addStyles.addComponent}>
+								const dt = new Date();
 
-          {/* Header */}
-          <div className={addStyles.header}>
-            <div>{lang.addBook}&nbsp;</div>
-            <div onClick={exit} className={addStyles.xButton}>
-              +
-            </div>
-          </div>
-          {/* Content */}
-          <div className={addStyles.content}>
-            <div className={addStyles.booksContainer}>
-              {/* List books */}
-              {Object.keys(books).map((keyName, key) => (
-                <div>
-                  <img
-                    key={key}
-                    className={addStyles.addImg}
-                    src={covers[parseInt(books[key])]}
-                    alt="BookCover"
-                    onClick={() => setSelectedBookID(books[key])}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-          {/* Footer */}
-          <div className={addStyles.footer}>
-            <button
-              // onClick push the slected book to the user books
-              onClick={() => {
-                const userBooksRef = firebase.database().ref("users/0/books");
-                const dt = new Date();
-                userBooksRef.child(selectedBookID).set({
-                  bookID: selectedBookID,
-                  finishDate: `${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()}`,
-                  note: "",
-                  stars: "0",
-                  startDate: `${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()}`,
-                });
-                setSelectedBookID("");
-                exit();
-              }}
-              // Cant click the button until a book is selected
-              disabled={selectedBookID === "" ? true : false}
-            >
-              {lang.add}
-            </button>
-          </div>
-        </div>
-      )}
-    </>
-  );
+								userBooksRef.child(selectedBookID).set({
+									bookID: selectedBookID,
+									finishDate: `${dt.getFullYear()}-${
+										dt.getMonth() + 1
+									}-${dt.getDate()}`,
+									note: "",
+									stars: "0",
+									startDate: `${dt.getFullYear()}-${
+										dt.getMonth() + 1
+									}-${dt.getDate()}`,
+								});
+
+								setSelectedBookID("");
+								exit();
+							}}
+							disabled={selectedBookID === "" ? true : false}
+						>
+							{lang.add}
+						</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default Add;
--- src/Components/App.js
+++ src/Components/App.js
@@ -108,97 +108,145 @@ function App() {
 		pullData();
 	}, [showEdit, showAdd]);
 
+	function returnEdit() {
+		appStyle = appStyles.appUnscrollable;
+		return (
+			<>
+				<div
+					className={appStyles.darkOverlay}
+					onClick={() => {
+						setShowEdit(false);
+						appStyle = appStyles.appScrollable;
+					}}
+				/>
+				<Edit
+					exit={setShowEditFunc}
+					booksData={allBooks[editBookID]}
+					usersData={userBooks[editBookID]}
+					bookID={editBookID}
+					lang={language}
+				/>
+			</>
+		);
+	}
+
+	function returnAdd() {
+		appStyle = appStyles.appUnscrollable;
+		return (
+			<>
+				<div
+					className={appStyles.darkOverlay}
+					onClick={() => {
+						setShowAdd(false);
+						appStyle = appStyles.appScrollable;
+					}}
+				/>
+				<Add
+					exit={setShowAddFunc}
+					booksData={allBooks}
+					usersData={userBooks}
+					lang={language}
+				/>
+			</>
+		);
+	}
+
+	function returnYears() {
+		return (
+			<div className={appStyles.years}>
+				{yearsArray.map((keyName, key) => (
+					<div key={key}>
+						<div
+							className={
+								activeFilter === yearsArray[key]
+									? appStyles.yearsFocus
+									: appStyles.year
+							}
+							onClick={() => {
+								setActiveFilter(yearsArray[key]);
+								appStyle = appStyles.appScrollable;
+							}}
+						>
+							<>{keyName}</>
+						</div>
+					</div>
+				))}
+			</div>
+		);
+	}
+
+	function returnBooks() {
+		results = 0;
+		return (
+			<div className={appStyles.bookCovers}>
+				{/* Show All books if we are on all, or only show years selected year books */}
+				{Object.keys(books).map(
+					(keyName, key) =>
+						("All" === activeFilter ||
+							books[keyName].finishDate.slice(0, 4) ===
+								activeFilter) &&
+						((results += 1),
+						(
+							// onClick open Edit component
+							<div
+								className={appStyles.book}
+								key={key}
+								onClick={() => {
+									editBookID = books[keyName].bookID;
+									setShowEdit(true);
+								}}
+							>
+								<img
+									src={books[keyName].cover}
+									alt="BookCover"
+								/>
+								{/* Stars */}
+								<div>
+									{[...Array(parseInt(books[keyName].stars))].map(() => "★")}
+									{[...Array(5 - parseInt(books[keyName].stars))].map(() => "☆")}
+								</div>
+							</div>
+						))
+				)}
+				<div
+					// onClick open Add component
+					className={appStyles.addBook}
+					onClick={() => setShowAdd(true)}
+				>
+					<div className={appStyles.addCenter} />
+				</div>
+			</div>
+		);
+	}
+
 	return (
 		<>
 			{/* Wait till tha data arrives */}
 			{Object.keys(books).length !== 0 && (
 				<div className={appStyle}>
-					{showAdd === true &&
-						((appStyle = appStyles.appUnscrollable),
-						(
-							<>
-								{/* Dark overlay */}
-								<div
-									className={appStyles.darkOverlay}
-									onClick={() => {
-										setShowAdd(false);
-										appStyle = appStyles.appScrollable;
-									}}
-								/>
-								{/* Add component */}
-								<Add
-									exit={setShowAddFunc}
-									booksData={allBooks}
-									usersData={userBooks}
-									lang={language}
-								/>
-							</>
-						))}
-
-					{showEdit === true &&
-						((appStyle = appStyles.appUnscrollable),
-						(
-							<>
-								{/* Dark overlay */}
-								<div
-									className={appStyles.darkOverlay}
-									onClick={() => {
-										setShowEdit(false);
-										appStyle = appStyles.appScrollable;
-									}}
-								/>
-								{/* Edit component */}
-								<Edit
-									exit={setShowEditFunc}
-									booksData={allBooks[editBookID]}
-									usersData={userBooks[editBookID]}
-									bookID={editBookID}
-									lang={language}
-								/>
-							</>
-						))}
-					{/* Header */}
+					{showEdit === true && returnEdit()}
+					{showAdd === true && returnAdd()}
 					<div className={appStyles.header}>
 						<div className={appStyles.menu}>
-							{/* Logo */}
-							<p unselectable="on">BookShelf</p>
-							{/* Language selector */}
-							<div className={appStyles.dropdown}>
-								<div className={appStyles.activeLang}>
-									<img
-										src={language.flag}
-										alt="languageFlag"
-										className={appStyles.flag}
-									/>
-								</div>
-								<div
-									className={appStyles.lang}
+							<p unselectable="on" className={appStyles.logo}>
+								BookShelf
+							</p>
+							<div className={appStyles.lang}>
+								<img
 									onClick={() => setLanguage(words.hu)}
-								>
-									<img
-										src={huFlag}
-										alt="huFlag"
-										className={appStyles.flag}
-									/>
-								</div>
-								<div
-									className={appStyles.lang}
-									id={appStyles.en}
+									src={huFlag}
+									alt="huFlag"
+								/>
+								<img
 									onClick={() => setLanguage(words.en)}
-								>
-									<img
-										src={enFlag}
-										alt="enFlag"
-										className={appStyles.flag}
-									/>
-								</div>
+									src={enFlag}
+									alt="enFlag"
+								/>
 							</div>
 						</div>
 					</div>
-					{/* Content */}
 					<div className={appStyles.content}>
 						<div className={appStyles.options}>
-							{/* Filters */}
 							<div
 								className={
 									activeFilter !== "All"
@@ -221,32 +269,7 @@ function App() {
 							</div>
 						</div>
 						<div className={appStyles.divider} />
-						{/* List of years */}
-						{activeFilter !== "All" && (
-							<div className={appStyles.years}>
-								{yearsArray.map((keyName, key) => (
-									<div key={key}>
-										<div
-											className={
-												activeFilter === yearsArray[key]
-													? appStyles.yearsFocus
-													: appStyles.year
-											}
-											onClick={() => {
-												setActiveFilter(
-													yearsArray[key]
-												);
-												appStyle =
-													appStyles.appScrollable;
-											}}
-										>
-											<>{keyName}</>
-										</div>
-									</div>
-								))}
-							</div>
-						)}
-						{/* Number of results */}
+						{activeFilter !== "All" && returnYears()}
 						<div className={appStyles.bookResults}>
 							<div className={appStyles.activeYear}>
 								{activeFilter === "All"
@@ -258,73 +281,7 @@ function App() {
 								{results}
 							</div>
 						</div>
-						{
-							/* Books Container */
-							((results = 0), //reset the results number
-							(
-								<div className={appStyles.bookCovers}>
-									{/* Show All books if we are on all, or only show years selected year books */}
-									{Object.keys(books).map(
-										(keyName, key) =>
-											("All" === activeFilter ||
-												books[keyName].finishDate.slice(
-													0,
-													4
-												) === activeFilter) &&
-											((results += 1),
-											(
-												// onClick open Edit component
-												<div
-													className={appStyles.book}
-													key={key}
-													onClick={() => {
-														editBookID =
-															books[keyName]
-																.bookID;
-														setShowEdit(true);
-													}}
-												>
-													<img
-														src={
-															books[keyName].cover
-														}
-														alt="BookCover"
-													/>
-													{/* Stars */}
-													<div>
-														{[
-															...Array(
-																parseInt(
-																	books[
-																		keyName
-																	].stars
-																)
-															),
-														].map(() => "★")}
-														{[
-															...Array(
-																5 -
-																	parseInt(
-																		books[
-																			keyName
-																		].stars
-																	)
-															),
-														].map(() => "☆")}
-													</div>
-												</div>
-											))
-									)}
-									<div
-										// onClick open Add component
-										className={appStyles.addBook}
-										onClick={() => setShowAdd(true)}
-									>
-										<div className={appStyles.addCenter} />
-									</div>
-								</div>
-							))
-						}
+						{returnBooks()}
 					</div>
 				</div>
 			)}
